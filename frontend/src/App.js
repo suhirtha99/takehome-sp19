229dae1e898c9bf4d5bf02744d1167f70fba8576
suhirtha99 <sraj6@illinois.edu>
2019-01-27 19:00:07 -0600

    Part 1 and local host running.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Instructions />
+        <Instructions complete={'complete'} />
         {this.state.shows.map(x => (
           <Show id={x.id} name={x.name} episodes_seen={x.episodes_seen} />
         ))}
