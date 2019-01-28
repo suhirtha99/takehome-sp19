2aa2daee7adbd494732d45c3231a37e56180c802
suhirtha99 <sraj6@illinois.edu>
2019-01-27 19:03:14 -0600

    /shows/id endpoint defined

--- backend/app.py
+++ backend/app.py
@@ -65,6 +65,12 @@ def delete_show(id):
 
 # TODO: Implement the rest of the API here!
 
+@app.route("/shows/<id>", methods=['GET'])
+def get_show_by_id(id):
+    if db.getById('shows', int(id)) is None:
+        return create_response(status=404, message="No show with this id exists.")
+    return create_response({"show": db.getById('shows', int(id))})
+
 """
 ~~~~~~~~~~~~ END API ~~~~~~~~~~~~
 """
